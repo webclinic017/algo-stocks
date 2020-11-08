31e94b36a0671beab62555df8907c95e22be9b6a
zuongthao <zuongthao@gmail.com>
2020-11-08 11:07:42 +0700

    copy testcase files from other repo

--- testcases/StockBreakOut/test01.py
+++ testcases/StockBreakOut/test01.py
@@ -1,12 +1,17 @@
 import os
-from backtesting import Backtest, Strategy
-from backtesting.magnus import _read_file
+import sys
+
+BACKTESTING_MODULE_PATH = os.path.abspath('../../backtest/backtesting/')
+sys.path.insert(1, BACKTESTING_MODULE_PATH)
+from backtest.backtesting import Backtest, Strategy
 
 import method.Ticker as Ticker
 import method.TakeProfit as TakeProfit
 import method.CutLoss as CutLoss
-
+from method.data import get_pricing_by_path
 path = os.getcwd()
+
+
 class StockBreakOut(Strategy):
     def init(self):
         self.buy_price = 0
@@ -21,15 +26,15 @@ class StockBreakOut(Strategy):
             if self.buy_price != 0 and (TakeProfit.takeProfit(5, last_price, self.buy_price) or CutLoss.shouldCutLossByPercent(8, last_price, self.buy_price)):
                 self.position.close()
                 self.buy_price = 0
+
 vn30_ticker = Ticker.getListBlueChips2020()
+DATA_PATH = os.path.abspath('../../vn-stock-data/VNX/')
 for ticker in vn30_ticker:
-    ticker_data = _read_file(ticker+'.csv')
+    ticker_data = get_pricing_by_path(DATA_PATH+ticker+'.csv')
     bt = Backtest(ticker_data, StockBreakOut, commission=.005, exclusive_orders=False)
     stats = bt.run()
     # bt.plot()
     # print(stats)
-    # print(ticker)
-    # print(stats['Sharpe Ratio'])
-    # # print(stats['_trades'])
-    new_file = path+"/result_"+ticker+".csv"
-    stats['_trades'].to_csv(new_file, index=False)
+    print(ticker)
+    print(stats['Sharpe Ratio'])
+
