b803690304843ebde38774f5b55680038b5e8be6
zuongthao <zuongthao@gmail.com>
2020-11-12 09:33:50 +0700

    update some file

--- testcases/test.py
+++ testcases/test.py
@@ -1,4 +1,10 @@
-import numpy as np
-a = [[1, 2, 3]]
-a = np.append(a, [[7, 8, 9]], axis=0)
-print(a)
+import os
+import method.SaleRules as _sr
+import method.algofuncs as _af
+
+DATA_PATH = os.path.abspath('../vn-stock-data/VNX/')
+ticker_data = _af.get_pricing_by_path(DATA_PATH + '/' + 'PNJ.csv')
+# print(ticker_data)
+look_back = 10
+look_back_extra = 15
+print(_sr.getATR(look_back, ticker_data.High[-look_back_extra:-1], ticker_data.Low[-look_back_extra:-1]))
