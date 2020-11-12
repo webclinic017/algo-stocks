2e7ed75aacfcb403d2cd8b3135ac786ddb0a5936
zuongthao <zuongthao@gmail.com>
2020-11-12 15:03:16 +0700

    update some files

--- testcases/test.py
+++ testcases/test.py
@@ -6,5 +6,5 @@ DATA_PATH = os.path.abspath('../vn-stock-data/VNX/')
 ticker_data = _af.get_pricing_by_path(DATA_PATH + '/' + 'PNJ.csv')
 # print(ticker_data)
 look_back = 10
-look_back_extra = 15
+look_back_extra = 11
 print(_sr.getATR(look_back, ticker_data.High[-look_back_extra:-1], ticker_data.Low[-look_back_extra:-1]))
