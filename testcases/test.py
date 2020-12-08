37af70cd7abca58edb7ca2afac5016b9128e12ff
zuong thao <zuongthao.tn@gmail.com>
2020-12-08 17:57:10 +0700

    add some tests

--- testcases/test.py
+++ testcases/test.py
@@ -1,12 +1,61 @@
 import os
+import sys
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
+METHOD_MODULE_PATH = os.path.abspath('..')
+sys.path.insert(1, METHOD_MODULE_PATH)
 import method.SaleRules as _sr
 import method.algofuncs as _af
 
-a = range(10, 35, 5)
-for n in a:
-  print(n)
+
+DATA_PATH = os.path.abspath('../vn-stock-data/VNX/')
+ticker_data = _af.get_pricing_by_path(DATA_PATH + '/' + 'KOS.csv', start_date='2018-11-01')
+
+# plt.figure()
+# plt.subplot(211)
+# Vol = _af.getVolatility(ticker_data["Close"], 252)
+# # Vol2 = ticker_data["High"] - ticker_data["Low"]
+# plt.plot(ticker_data["Close"], label='Close Price', color='black')
+# plt.plot(ticker_data["High"], label='High Price')
+# plt.plot(ticker_data["Low"], label='Low Price')
+# plt.legend()
+# plt.subplot(212)
+# plt.plot(Vol, label='Volatility')
+# plt.legend()
+# plt.show()
+
+# plt.plot(ticker_data["High"], label='High Price', linestyle = 'dotted')
+# plt.plot(ticker_data["Low"], label='Low Price', linestyle = 'dotted')
+# plt.legend()
+# plt.show()
+
+# Vol = ticker_data["High"] - ticker_data["Low"]
+# print(Vol.tail())
+# plt.plot(Vol, label='Volatility')
+# plt.legend()
+# plt.show()
+
+## Computing Volatility
+# Compute the logarithmic returns using the Closing price
+# ticker_data['Log_Ret'] = np.log(ticker_data['Close'] / ticker_data['Close'].shift(1))
+# Compute Volatility using the pandas rolling standard deviation function
+# ticker_data['Volatility'] = ticker_data['Log_Ret'].rolling(window=252).std() * np.sqrt(252)
+# Plot the ticker_data Price series and the Volatility
+# ticker_data[['Close', 'Volatility']].plot(subplots=True, color='blue',figsize=(8, 6))
+# plt.figure()
+# plt.subplot(211)
+# plt.plot(ticker_data[['Close']])
+# plt.subplot(212)
+# plt.plot(ticker_data[['Volatility']])
+# plt.show()
+
+
+
+# a = range(10, 35, 5)
+# for n in a:
+#   print(n)
 
 # DATA_PATH = os.path.abspath('../vn-stock-data/VNX/')
 # ticker_data = _af.get_pricing_by_path(DATA_PATH + '/' + 'PNJ.csv')
