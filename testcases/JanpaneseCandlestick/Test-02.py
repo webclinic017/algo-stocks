ff29d595babdc9e4441162b4fcf2d63bcb5d0ca4
zuongthao <zuongthao@gmail.com>
2021-07-16 18:21:37 +0700

    update test file

--- testcases/JanpaneseCandlestick/Test-02.py
+++ testcases/JanpaneseCandlestick/Test-02.py
@@ -1,6 +1,8 @@
 import warnings
 warnings.filterwarnings('ignore')
 
+import datetime
+import numpy
 import os
 import sys
 METHOD_MODULE_PATH = os.path.abspath('../..')
@@ -28,17 +30,28 @@ class Test(Strategy):
             ups = self.data.UpShadow
             lows = self.data.LowerShadow
             height = self.data.Height
+            dates = self.data.Date
+            current_date = numpy.datetime_as_string(dates[-1], unit="D")
 
             isUmbrellaCandlestick = jModel.isUmbrellaCandlestick(bodies[-2], height[-2], ups[-2], lows[-2])
             isWhiteCandlestick = jModel.isWhiteCandlestick(opens[-1], prices[-1])
             isBlackCandlestick = jModel.isBlackCandlestick(opens[-1], prices[-1])
-            if isUmbrellaCandlestick is True and isWhiteCandlestick is True and prices[-1] > prices[-2]:
+            isShavenHead = jModel.isShavenHead(height[-1], ups[-1])
+            if isUmbrellaCandlestick is True and isWhiteCandlestick is True and prices[-1] > prices[-2] and isShavenHead is True:
                 self.buy(sl=0.9*prices[-1], tp=1.2 * prices[-1])
+                # self.buy(sl=0.9*prices[-1])
             # if isUmbrellaCandlestick is True and isBlackCandlestick is True:
             #     self.position.close()
 
+            py_date = datetime.datetime.strptime(current_date, "%Y-%m-%d")
+            week_day = py_date.weekday()
+            day = py_date.day
+            if day > 22 and week_day > 2:
+                self.position.close()
+
+
 DATA_PATH = os.path.abspath('../../vn-stock-data/VNX/')
-ticker_id = 'VCB'
+ticker_id = 'BVH'
 ticker_data = _af.get_pricing_by_path(DATA_PATH + '/' + ticker_id + '.csv', '2010-01-01')
 new_data = jModel.convertToJapanCandle(ticker_data)
 bt = Backtest(ticker_data, Test, commission=.005, exclusive_orders=False)
